bcc65082644757a3ff3a862ee133fe1d820ff2a3
victor <ruizdvictor@gmail.com>
2019-09-15 12:48:28 -0400

    Created photo object

---
+++ Colors/Models/Photo.swift
@@ -0,0 +1,16 @@
+//
+//  Photo.swift
+//  Colors
+//
+//  Created by Victor  on 9/15/19.
+//  Copyright © 2019 Victor . All rights reserved.
+//
+
+import Foundation
+
+struct Photo {
+    var albumId: Int
+    var id: Int
+    var title: String
+    var url: String
+}
